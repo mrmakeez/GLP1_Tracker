abbbaa2f1f4b5e679a577fdd60701d05d742edc6
mrmakeez <mckerrow77@gmail.com>
2026-01-21 19:03:24 +1300

    Add GH Pages SPA fallback redirect (#31)

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import { restoreGhPagesRedirect } from './router/ghPagesRedirect'
+
+restoreGhPagesRedirect(import.meta.env.BASE_URL)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
